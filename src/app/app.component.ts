1b810ba93a328fba81c0bfb8d5a0c620d6f988a7
Kan Adachi <Kadachi1023@gmail.com>
2016-10-25 18:50:38 -0700

    :hammer: removed external templating

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  template: `
+    <h1>
+      {{title}}
+    </h1>
+  `
 })
 export class AppComponent {
   title = 'app works!';
